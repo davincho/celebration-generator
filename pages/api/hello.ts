dd52227c32f497d76dd3148e36388db752871e28
David Madner <david.madner@gmail.com>
2022-10-02 13:00:20 +0200

    try additional package

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,9 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
-}
+require('chrome-aws-lambda')
 
 import path from "path";
 
@@ -28,7 +26,7 @@ const start = async () => {
 
   // Extract all the compositions you have defined in your project
   // from the webpack bundle.
-  const comps = await getCompositions('./public/video/', {
+  const comps = await getCompositions(bundleLocation, {
     // You can pass custom input props that you can retrieve using getInputProps()
     // in the composition list. Use this if you want to dynamically set the duration or
     // dimensions of the video.
@@ -62,7 +60,7 @@ const start = async () => {
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
 
 
